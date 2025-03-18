a008dbf7c4414a3535504a0bd809dee4e372bf0a
Harsh260105 <harshpatel17079@gmail.com>
2025-03-18 19:25:56 +0530

    Fix: Enhance CORS configuration to specify allowed methods and headers

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,15 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "https://engineer-s-depot.vercel.app", credentials: true }));
+app.use(
+  cors({
+    origin: "https://engineer-s-depot.vercel.app", // Your frontend domain
+    credentials: true, // Allow cookies and authentication headers
+    methods: "GET,POST,PUT,DELETE", // Allowed methods
+    allowedHeaders: "Content-Type,Authorization", // Allowed headers
+  })
+);
+
 
 // API Routes
 app.use("/api/v1/user", userRoute);
