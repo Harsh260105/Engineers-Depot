1011fe5a5a561ef33072954386b1b17bd6a91a87
Harsh260105 <harshpatel17079@gmail.com>
2025-03-18 19:21:55 +0530

    Fix: Update CORS configuration to allow requests from the Vercel deployment URL

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "https://engineer-s-depot.vercel.app", credentials: true }));
 
 // API Routes
 app.use("/api/v1/user", userRoute);
